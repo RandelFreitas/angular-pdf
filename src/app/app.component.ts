7d7a1d412e3376ae6f5c5e0fbac353fc9dfb9969
randel <randelfreitas@gmail.com>
2021-08-31 15:16:30 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { jsPDF } from 'jspdf';
+
+// LINKS DE IDEIAS:
+// https://codepen.io/stuehler/pen/pZMdKo
+// http://raw.githack.com/MrRio/jsPDF/master/docs/
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('content', {static: false}) el!: ElementRef;
+
   title = 'testepdf';
+
+  printsimplePDF () {
+    let pdf = new jsPDF('p', 'pt', 'a4');
+    pdf.addFont("Arimo-Regular.ttf", "Arimo", "normal");
+    pdf.setFont("Arimo");
+    pdf.setFontSize(28);
+    pdf.html(this.el.nativeElement, {
+      callback: (pdf) => {
+        pdf.save("teste.pdf");
+      }
+    })
+  }
 }
