1c867846f0705dbeb91524415914c346634ea526
randel <randelfreitas@gmail.com>
2021-09-03 09:30:26 -0300

    Novas tentaivas html to pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { jsPDF } from 'jspdf';
+import { Anton } from '../assets/fonts/teste';
 
 // LINKS DE IDEIAS:
 // https://codepen.io/stuehler/pen/pZMdKo
 // http://raw.githack.com/MrRio/jsPDF/master/docs/
+// https://jsfiddle.net/cxLzsp4r/2/
 
 @Component({
   selector: 'app-root',
@@ -13,16 +15,26 @@ import { jsPDF } from 'jspdf';
 export class AppComponent {
   @ViewChild('content', {static: false}) el!: ElementRef;
 
-  title = 'testepdf';
+  title = 'testepdf'; 
 
   printsimplePDF () {
-    let pdf = new jsPDF('p', 'pt', 'a4');
-    pdf.addFont("Arimo-Regular.ttf", "Arimo", "normal");
-    pdf.setFont("Arimo");
-    pdf.setFontSize(28);
-    pdf.html(this.el.nativeElement, {
-      callback: (pdf) => {
-        pdf.save("teste.pdf");
+    const doc = new jsPDF('p', 'pt', 'a4');
+
+    doc.addFileToVFS('Anton-Regular.ttf', Anton);
+    doc.addFont('Anton-Regular.ttf', 'Anton', 'normal');
+    doc.setFont('Anton');
+    doc.setFontSize(12);
+
+    doc.text("Teste fonte fora do html!", 100, 100);
+
+    console.log(this.el.nativeElement);
+
+    doc.html(this.el.nativeElement, {
+      callback: (doc) => {
+        // doc.addFileToVFS('Anton-Regular.ttf', Anton); // TENTATIVAS EM VÃO DE INSERÇÃO DA FONTE NO HTML
+        // doc.addFont('Anton-Regular.ttf', 'Anton', 'normal');
+        // doc.setFont('Anton');
+        doc.save("teste.pdf");
       }
     })
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
